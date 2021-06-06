2bd41249f9baaab76667ac57812563bc935b3e00
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-06-06 15:43:51 +0200

    products_table: naam kolom verwijderd
    Naam kolom verwijderd uit tabel, dit was in het excel bestand de sub-categorie. In plaats van een naam kunnen we gebruik maken van het merk + model naam.

--- database/migrations/2021_06_04_121552_create_products_table.php
+++ database/migrations/2021_06_04_121552_create_products_table.php
@@ -15,7 +15,6 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('brand');
             $table->string('model');
             $table->integer('price');
