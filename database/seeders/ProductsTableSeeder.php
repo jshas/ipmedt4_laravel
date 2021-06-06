e0421cb33098869235786438bf21cab5a2ca1eb9
Niels <s1122255@student.hsleiden.nl>
2021-06-06 15:15:59 +0200

    onnodige comments weg gehaald

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -14,20 +14,6 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        /*$table->id();
-        $table->string('name');
-        $table->string('brand');
-        $table->string('model');
-        $table->integer('price');
-        $table->string('description');
-        $table->string('img_name');
-        $table->string('img_path')->default('/uploads/placeholder.png');
-        $table->string("category")->references('category')->on('category');
-        $table->string("sub_category")->references('sub_category')->on('sub_category');
-        $table->integer('limit')->default(1);
-        $table->boolean('permission')->default(true);
-        $table->integer('rule')->references('id')->on('rules');
-        */
         DB::table('products')->insert([
           "name" => "Headset",
           "brand" => "Poly",
