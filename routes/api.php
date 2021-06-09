59f2b8adb3253a4cde7f07f254a03d1e35651461
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-06-09 12:52:26 +0200

    img_path: 'public/' verwijderd

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/products/', 'App\Http\Controllers\ProductController@index');
+Route::get('/products/', 'App\Http\Controllers\ProductController@index'); 
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show');
