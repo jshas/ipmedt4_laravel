e875cc29d491e6a86134ba0b1ddd1a39ae56e5d6
s1123069 <s1123069@student.hsleiden.nl>
2021-06-11 18:18:11 +0200

    producten worden aan database toegevoegd. er wordt gelet op limiet.
    Co-authored-by: Niels <s1122255@student.hsleiden.nl>

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,13 +14,27 @@ class OrderController extends Controller
     public function plaatsOrder($user_id, $product_id){
         $product_price = Product::all()->where("id", "==", $product_id)->first()->price;
 
-        DB::table('orders')->insert([
-            'user_id' => $user_id,
-            'product_id' => $product_id,
-            'price' => $product_price,
-            'quantity' => 1,
-        ]);
+        // deze functie checkt hoevaak de gebruiker hetzelde item heeft besteld
+        $user_frequentie = Order::all()->where("user_id", "==", $user_id)->where("product_id", "==", $product_id)->count();
+        
+        // deze functie checkt de bestel-limit van het product
+        $bestel_limit = Product::all()->where("id", "==", $product_id)->first()->limit;
 
-        return ("besteld");
+        if ( $user_frequentie < $bestel_limit) {
+            DB::table('orders')->insert([
+                'user_id' => $user_id,
+                'product_id' => $product_id,
+                'price' => $product_price,
+                'quantity' => 1,
+            ]);
+
+            return ("besteld");
+        }
+
+        else{
+            return ("over limiet, niet besteld");
+        }
+        
+        // return ($bestel_limit);
     }
 }
