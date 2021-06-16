2af5c96272414c69eb2395ed1df37a5fc21c23fd
Niels <s1122255@student.hsleiden.nl>
2021-06-16 12:29:32 +0200

    categorie controller geupdate

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,8 +9,28 @@ use DB;
 
 class CategoryController extends Controller
 {
-    public function categoriseerProducten ($title){
-      $product_list = Product::all()->where("category", "==", $title);
+    public function CategorieCommunicatie(){
+      $product_list = Product::all()->where("category", "==", "Communicatie");
+      return $product_list;
+    }
+    public function CategorieErgonomie(){
+      $product_list = Product::all()->where("category", "==", "Ergonomie");
+      return $product_list;
+    }
+    public function CategorieKabel(){
+      $product_list = Product::all()->where("category", "==", "Kabel");
+      return $product_list;
+    }
+    public function CategoriePrint(){
+      $product_list = Product::all()->where("category", "==", "Print");
+      return $product_list;
+    }
+    public function CategorieRandapparatuur(){
+      $product_list = Product::all()->where("category", "==", "Randapparatuur");
+      return $product_list;
+    }
+    public function CategorieWerkplek(){
+      $product_list = Product::all()->where("category", "==", "Werkplek");
       return $product_list;
     }
 }
