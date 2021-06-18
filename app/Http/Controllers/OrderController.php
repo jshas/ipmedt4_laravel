939ef050256bd195b0ecfd4d6c986ebb2f57d07e
s1123069 <s1123069@student.hsleiden.nl>
2021-06-18 18:19:18 +0200

    order post request is af. alle items worden correct verwerkt. Je krijgt als response op react een array met string. Strings geven met productaam aan of het product is besteld. zo ja, met welke regel. zo nee, geeft de string aan dat het limiet is bereikt van het product

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,128 +15,131 @@ class OrderController extends Controller
     public function store(Request $request){
         $user_id = $request->user_id ;
         $product_id_array = $request->product_id_array;
-        $teller = 0;
 
         $response_array = [];
 
         for ($index=0; $index < count($product_id_array) ; $index++) { 
-            $teller++;
+            $product_price = Product::all()->where("id", "==", $product_id_array[$index])->first()->price;
+            $product_name = Product::all()->where("id", "==", $product_id_array[$index])->first()->sub_category;
+
+            // deze functie checkt hoevaak de gebruiker hetzelde item heeft besteld
+            $user_frequentie = Order::all()->where("user_id", "==", $user_id)->where("product_id", "==", $product_id_array[$index]);
+            $user_frequentie_count = $user_frequentie->count();
+
+            //deze functie checkt hoevaak de gebruiker hetzelde item heeft besteld in het huidige jaar
+            $huidig_jaar = Carbon::now()->year;
+            $user_frequentie_jaarlijks = $user_frequentie->where("created_year", "==", $huidig_jaar);
+            $user_frequentie_jaarlijks_count = $user_frequentie_jaarlijks->count();
+
+            // deze functie checkt de bestel-limit van het product
+            $bestel_limit = Product::all()->where("id", "==", $product_id_array[$index])->first()->limit;
+
+            // $product_rule = DB::table('products')->join('rules', 'products.rule_id', '=', 'rules.id')->get()->rules.id;
+            $product_rule_id = Product::all()->where("id", "==", $product_id_array[$index])->first()->rule_id;
+
+
+            $rule_1 = Rule::all()->where("id", "==", "1")->first();
+            $rule_2 = Rule::all()->where("id", "==", "2")->first();
+            $rule_3 = Rule::all()->where("id", "==", "3")->first();
+            $rule_4 = Rule::all()->where("id", "==", "4")->first();
+
+            
+
+
+            if ($product_rule_id == 1) {
+                if ($user_frequentie_count < $rule_1->total_limit) {
+
+                    // hier de code om te bestellen
+                    DB::table('orders')->insert([
+                        'user_id' => $user_id,
+                        'product_id' => $product_id_array[$index],
+                        'price' => $product_price,
+                    ]);
+
+                    $response_array[$index] = $product_name . " in de database gezet met regel 1";
+                }
+                $response_array[$index] = "Limiet voor ". $product_name . " al bereikt";
+            }
+
+
+            elseif ($product_rule_id == 2) {
+                //hoeveel bestellingen van het product zijn er dit jaar gemaakt.
+                //als het minder is dan het limit in het huidige jaar
+                //voeg dan het product 1 keer toe
+                if ($user_frequentie_jaarlijks_count < $rule_2->yearly_limit) {
+                    
+                    // hier de code om te bestellen
+                    DB::table('orders')->insert([
+                        'user_id' => $user_id,
+                        'product_id' => $product_id_array[$index],
+                        'price' => $product_price,
+                    ]);
+
+                    $response_array[$index] = $product_name . "In de database gezet met regel 2";
+                }
+
+                $response_array[$index] = "Limiet voor ". $product_name . " al bereikt";
+            }
+
+
+            elseif ($product_rule_id == 3) {
+                if ($user_frequentie_jaarlijks_count < $rule_3->yearly_limit){
+
+                    // hier de code om te bestellen
+                    DB::table('orders')->insert([
+                        'user_id' => $user_id,
+                        'product_id' => $product_id_array[$index],
+                        'price' => $product_price,
+                    ]);
+
+                    $response_array[$index] = $product_name . "In de database gezet met regel 3";
+                }
+                
+                $response_array[$index] = "Limiet voor ". $product_name . " al bereikt";
+            }
+            
+
+            elseif ($product_rule_id == 4) {
+                if ($user_frequentie_jaarlijks_count < $rule_4->yearly_limit){
+
+                    // hier de code om te bestellen
+                    DB::table('orders')->insert([
+                        'user_id' => $user_id,
+                        'product_id' => $product_id_array[$index],
+                        'price' => $product_price,
+                    ]);
+
+                    $response_array[$index] = $product_name . "In de database gezet met regel 4";
+                }
+                
+                $response_array[$index] = "Limiet voor ". $product_name . " al bereikt";
+            }
+
+            elseif ($product_rule_id == null) {
+                DB::table('orders')->insert([
+                    'user_id' => $user_id,
+                    'product_id' => $product_id_array[$index],
+                    'price' => $product_price,
+                ]);
+
+                $response_array[$index] = $product_name . " In de database gezet, product heeft geen regel";
+            }
+            
         }
         return $response_array;
 
-        $product_price = Product::all()->where("id", "==", $product_id)->first()->price;
-
-        // deze functie checkt hoevaak de gebruiker hetzelde item heeft besteld
-        $user_frequentie = Order::all()->where("user_id", "==", $user_id)->where("product_id", "==", $product_id);
-        $user_frequentie_count = $user_frequentie->count();
-
-        //deze functie checkt hoevaak de gebruiker hetzelde item heeft besteld in het huidige jaar
-        $huidig_jaar = Carbon::now()->year;
-        $user_frequentie_jaarlijks = $user_frequentie->where("created_year", "==", $huidig_jaar);
-        $user_frequentie_jaarlijks_count = $user_frequentie_jaarlijks->count();
-
-        // deze functie checkt de bestel-limit van het product
-        $bestel_limit = Product::all()->where("id", "==", $product_id)->first()->limit;
-
-        // $product_rule = DB::table('products')->join('rules', 'products.rule_id', '=', 'rules.id')->get()->rules.id;
-        $product_rule_id = Product::all()->where("id", "==", $product_id)->first()->rule_id;
-
-
-        $rule_1 = Rule::all()->where("id", "==", "1")->first();
-        $rule_2 = Rule::all()->where("id", "==", "2")->first();
-        $rule_3 = Rule::all()->where("id", "==", "3")->first();
-        $rule_4 = Rule::all()->where("id", "==", "4")->first();
-
-
-        if ($product_rule_id == 1) {
-            if ($user_frequentie_count < $rule_1->total_limit) {
-
-                // hier de code om te bestellen
-                DB::table('orders')->insert([
-                    'user_id' => $user_id,
-                    'product_id' => $product_id,
-                    'price' => $product_price,
-                ]);
-                
-
-                return ("In de database gezet met regel 1");
-            }
-            return ("limiet al bereikt");
-        }
-
-
-        elseif ($product_rule_id == 2) {
-            //hoeveel bestellingen van het product zijn er dit jaar gemaakt.
-            //als het minder is dan het limit in het huidige jaar
-            //voeg dan het product 1 keer toe
-            if ($user_frequentie_jaarlijks_count < $rule_2->yearly_limit) {
-                
-                // hier de code om te bestellen
-                DB::table('orders')->insert([
-                    'user_id' => $user_id,
-                    'product_id' => $product_id,
-                    'price' => $product_price,
-                ]);
-
-                return("In de database gezet met regel 2");
-            }
-
-            return("limiet al bereikt");
-        }
-
-
-        elseif ($product_rule_id == 3) {
-            if ($user_frequentie_jaarlijks_count < $rule_3->yearly_limit){
-
-                // hier de code om te bestellen
-                DB::table('orders')->insert([
-                    'user_id' => $user_id,
-                    'product_id' => $product_id,
-                    'price' => $product_price,
-                ]);
-
-                return ("In de database gezet met regel 3");
-            }
-            
-            return("limiet al bereikt");
-        }
         
 
-        elseif ($product_rule_id == 4) {
-            if ($user_frequentie_jaarlijks_count < $rule_4->yearly_limit){
 
-                // hier de code om te bestellen
-                DB::table('orders')->insert([
-                    'user_id' => $user_id,
-                    'product_id' => $product_id,
-                    'price' => $product_price,
-                ]);
 
-                return ("In de database gezet met regel 4");
-            }
-            
-            return("limiet al bereikt");
-        }
-
-        elseif ($product_rule_id == null) {
-            DB::table('orders')->insert([
-                'user_id' => $user_id,
-                'product_id' => $product_id,
-                'price' => $product_price,
-            ]);
-
-            return ("In de database gezet, product heeft geen regel");
-        }
-
-        return (Product::all()->where("id", "==", $product_id)->first());
-
+        // return (Product::all()->where("id", "==", $product_id_array[$index])->first());
 
 
         // Dit is voorbeeldcode om te bestellen
 
         // DB::table('orders')->insert([
         //     'user_id' => $user_id,
-        //     'product_id' => $product_id,
+        //     'product_id' => $product_id_array[$index],
         //     'price' => $product_price,
         // ]);
 
