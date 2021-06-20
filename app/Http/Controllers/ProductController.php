5d439ed9af93eebf60c729f3a9cf1fe7bec74ea5
Jamal <62602628+jshas@users.noreply.github.com>
2021-06-20 03:46:17 +0200

    ProductController update: > Totaal aantal bestellingen nu beschikbaar per product wannneer de user ingelogd is. > V.b.: Dit kan gebruikt worden voor een dynamische limiet weergave per productkaart voor de ingelogde user.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,13 +12,15 @@ use Auth;
 
 
 class ProductController extends Controller{
+
     public function index(){
         if(Auth::user()->id){
             $products = Product::with('rule')->get()->all();
-            $user_id = Auth::user()->id;
-            return [$products, $user_id];
-        }
 
+            $user_id = Auth::user()->id;
+            $orders = Order::all()->where('user_id', '=', $user_id)->countBy('product_id')->sortKeys(); // Provides a count of products ordered by the user
+            return [$products, $user_id, $orders];
+        }
     }
 
     public function show($id){
